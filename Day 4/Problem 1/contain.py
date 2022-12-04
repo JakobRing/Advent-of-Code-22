0e73fb9689f18c89e404aaeb486377f1b34cc0d9
Jakob Ring <49451632+JakobRing@users.noreply.github.com>
2022-12-04 14:52:24 +0100

    better split for first and second section...

--- Day 4/Problem 1/contain.py
+++ Day 4/Problem 1/contain.py
@@ -3,9 +3,7 @@ if __name__ == "__main__":
     contain = 0
     f = open("../sections.txt")
     for line in f:
-        sections = line.strip().strip("\n").split(",")
-        fst_section = sections[0].split("-")
-        snd_section = sections[1].split("-")
+        fst_section, snd_section = [section.split("-") for section in line.strip().strip("\n").split(",")]
         if int(snd_section[0]) <= int(fst_section[0]) <= int(snd_section[1]) \
            and int(snd_section[0]) <= int(fst_section[1]) <= int(snd_section[1]):           # first section in second
             contain += 1
--- Day 4/Problem 2/overlap.py
+++ Day 4/Problem 2/overlap.py
@@ -3,9 +3,7 @@ if __name__ == "__main__":
     overlap = 0
     f = open("../sections.txt")
     for line in f:
-        sections = line.strip().strip("\n").split(",")
-        fst_section = sections[0].split("-")
-        snd_section = sections[1].split("-")
+        fst_section, snd_section = [section.split("-") for section in line.strip().strip("\n").split(",")]
         if int(fst_section[1]) < int(snd_section[0]) or int(snd_section[1]) < int(fst_section[0]):
             # second section is "over" the first section or first section is "over" the second section
             continue
