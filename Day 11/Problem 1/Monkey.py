c339602af3436c8d23a809953ebf95b801e9e8a9
Jakob Ring <49451632+JakobRing@users.noreply.github.com>
2022-12-21 13:05:34 +0100

    solutions of day 11

---
+++ Day 11/Problem 1/Monkey.py
@@ -0,0 +1,35 @@
+from typing import Callable
+
+
+class Monkey:
+
+    def __init__(self, starting_items: [int], op: str, div_test: int):
+        self.items_inspected = 0
+        self.items = starting_items
+        self.op = op    # use eval()
+        self.div_test = lambda x: x % div_test == 0
+        self.monkey1 = None
+        self.monkey2 = None
+
+    def add_monkey_refs(self, monkey1, monkey2):
+        self.monkey1 = monkey1
+        self.monkey2 = monkey2
+
+    def add_item(self, item: int):
+        self.items.append(item)
+
+    def get_nr_items_inspected(self):
+        return self.items_inspected
+
+    def inspect(self):
+        while self.items:
+            old = self.items.pop(0)
+            curr_item = int(eval(self.op))   # something like "old +/*..."
+            curr_item = curr_item // 3
+            if self.div_test(curr_item) and self.monkey1:
+                self.monkey1.add_item(curr_item)
+            elif self.monkey2:
+                self.monkey2.add_item(curr_item)
+            else:
+                raise Exception("Monkeys not referenced")
+            self.items_inspected += 1
---
+++ Day 11/Problem 1/main.py
@@ -0,0 +1,56 @@
+from Monkey import Monkey
+import re
+
+if __name__ == "__main__":
+    f = open("monkey.txt")
+    monkey_list = []
+    i_monkey_refs = []
+    line = next(f, None)
+    while line:
+        first_line = re.search('Monkey .*:', line)  # match "Monkey i:"
+        if not first_line:
+            raise Exception("not a monkey def")
+        line = next(f, None)                        # match "  Starting items: a, b, ..."
+        if line:
+            starting_items = list(map(int, line.strip().strip("\n").replace(",", "").split(" ")[2:]))
+        else:
+            raise Exception("problem in the monkey def")
+        line = next(f, None)                        # match "  Operation: new = old "
+        if line:
+            line = line.strip("\n").strip()
+            prefix = re.search('Operation: new = ', line).group(0)
+            op = line.removeprefix(prefix).strip()
+        else:
+            raise Exception("problem in the monkey def")
+        line = next(f, None)                        # match: "  Test: divisible by int"
+        if line:
+            div_test = int(line.strip("\n").strip("").split(" ")[-1])
+        else:
+            raise Exception("problem in the monkey def")
+        line = next(f, None)                        # match: "  If true: throw to monkey i"
+        if line:
+            curr_monkey_ref = [int(line.strip("\n").strip().split(" ")[-1])]
+        else:
+            raise Exception("problem in the monkey def")
+        line = next(f, None)  # match: "  If false: throw to monkey i"
+        if line:
+            curr_monkey_ref.append(int(line.strip("\n").strip().split(" ")[-1]))
+            i_monkey_refs.append(curr_monkey_ref)
+        else:
+            raise Exception("problem in the monkey def")
+        line = next(f, None)  # match: "\n"
+        line = next(f, None)  # next monkey
+        monkey_list.append(Monkey(starting_items, op, div_test))
+        print(monkey_list)
+
+    for i in range(len(monkey_list)):
+        monkey_list[i].add_monkey_refs(monkey_list[i_monkey_refs[i][0]], monkey_list[i_monkey_refs[i][1]])
+    for _ in range(20):
+        for monkey in monkey_list:
+            monkey.inspect()
+    monkey_business = []
+    for i in range(len(monkey_list)):
+        monkey_business.append(monkey_list[i].get_nr_items_inspected())
+        print(f"Monkey {i} inspected items {monkey_list[i].get_nr_items_inspected()}")
+    monkey_business.sort()
+    print(f"monkey business: {monkey_business[-1] * monkey_business[-2]}")
